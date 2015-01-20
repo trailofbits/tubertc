b37b5cabcbda1645fd53ae7333757a79a61464bc
yying <andy@trailofbits.com>
2015-01-20 09:30:37 -0500

    Added options for force downloading node.js

--- run.sh
+++ run.sh
@@ -10,23 +10,39 @@ error () {
     printf "[error] $1\n"
 }
 
+help () {
+    printf "Usage: $0 [-h|--help] [-f|--force]\n"
+    printf "  -h,--help   Shows this screen\n"
+    printf "  -f,--force  Forces the download of a node.js instance\n"
+    exit 1
+}
+
 install_nodejs () {
     ARCH="`uname -m`"
     PLATFORM="`uname`"
     NODEJS_URL="null"
+    DOWNLOAD=0
+
+    if [ "$1" = "1" ]; then
+        DOWNLOAD=1
+    fi
     
-    log "Checking if node is installed..."
-    node -v 2>&1 > /dev/null
-    if [ "$?" = "0" ]; then
-        log "node is found and working"
-        log "Checking if npm is installed..."
-        npm -v 2>&1 > /dev/null
+    if [ "$DOWNLOAD" = "0" ]; then
+        log "Checking if node is installed..."
+        node -v 2>&1 > /dev/null
         if [ "$?" = "0" ]; then
-            log "npm is found and working"
-            log "No need to install node.js"
-            return 0
+            log "node is found and working"
+            log "Checking if npm is installed..."
+            npm -v 2>&1 > /dev/null
+            if [ "$?" = "0" ]; then
+                log "npm is found and working"
+                log "No need to install node.js"
+                return 0
+            fi
+            log "Failed to detect npm"
         fi
-        log "Failed to detect npm"
+    else
+        log "Ignoring node/npm command check, downloading node.js"
     fi
 
     log "Detecting architecture and platform..."
@@ -55,9 +71,13 @@ install_nodejs () {
 
     FILENAME="`basename $NODEJS_URL`"
     BASENAME=${FILENAME%.tar.gz}
-
+    
     if [ ! -d $BASENAME ]; then
         log "Cannot find $BASENAME directory in $PWD"
+        DOWNLOAD=1
+    fi
+    
+    if [ "$DOWNLOAD" = "1" ]; then
         log "Downloading node.js from $NODEJS_URL"
         curl $NODEJS_URL 2> /dev/null | tar zxf -
         log "Download complete, extracted into $PWD/$BASENAME"
@@ -70,8 +90,18 @@ install_nodejs () {
     return 1
 }
 
+if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
+    help
+fi
+
 log "Preparing to run TubeRTC..."
-install_nodejs
+
+if [ "$1" = "--force" ] || [ "$1" = "-f" ]; then
+    install_nodejs 1
+else
+    install_nodejs
+fi
+
 if [ "$?" = "1" ]; then
     log "Adding $NODEJS_ROOT/bin to PATH"
     PATH="$PATH:$NODEJS_ROOT/bin"
