12c189d1ceeec6f3a8545174459c5ce7adf2586e
Eric Weinstein <eric.q.weinstein@gmail.com>
2015-12-14 20:20:10 -0500

    Lint public/js/vtc.js

--- public/js/vtc.js
+++ public/js/vtc.js
@@ -1,94 +1,123 @@
-/* Abstracts easyrtc functionality to enable easy reimplementing of this using custom WebRTC framework.
- *
+/**
+ * @file Abstracts easyrtc functionality to enable easy reimplementing
+ * of this using custom WebRTC framework.
  * NOTE: This should be the only module that calls out to easyrtc API.
  *
- * Requires:
- *   easyrtc
- *   js/error.js
+ * @requires module:easyrtc
+ * @requires module:js/error
  */
+
+'use strict';
 
 // This is a constant that should not be changed unless the GUI has been made
 // to support more viewports. This number should include the user itself.
 var kMaxCallersPerRoom = 15;
 
+/**
+ * Creates a new instance of the VTC client.
+ *
+ * @param {String} myId - The peer's ID.
+ * @param {String} roomName - The room name.
+ * @param {Function} onErrorFn - Error callback.
+ * @class
+ */
 var VTCClient = function(myId, roomName, onErrorFn) {
     var _id = myId;
     var _room = roomName;
 
+    /**
+     * Gets the peer ID.
+     *
+     * @returns {String} The peer ID.
+     * @public
+     */
     this.getId = function() {
         return _id;
     };
 
+    /**
+     * Gets the room name.
+     *
+     * @returns {String} The room name.
+     * @public
+     */
     this.getRoomName = function() {
         return _room;
     };
 
-    /* Parameters:
-     *   id : String
-     *     The peer ID to be translated
+    /**
+     * Translates peer IDs to user names.
      *
-     * Translates peer IDs to user names
+     * @param {String} id - The peer ID
+     * to be translated.
+     * @returns {String} The user name
+     * for the provided peer ID.
+     * @public
      */
     this.idToName = function(id) {
         return easyrtc.idToName(id);
     };
 
-    /* Parameters:
-     *   id : String
-     *     The peer ID of the other user/
+    /**
+     * Returns the status of the p2p connection
+     * between the current user and the specified user.
      *
-     * Returns the status of the p2p connection between the current user and the specified user.
+     * @param {String} id - The peer ID of the other user.
+     * @returns {String} The status of the p2p connection between
+     * the current user and the specified user. One of
+     * NOT_CONNECTED | BECOMING_CONNECTED | IS_CONNECTED
      */
     this.getConnectStatus = function(id) {
         return easyrtc.getConnectStatus(id);
     };
 
-    /* Parameters:
-     *   kbitsPerSecond : integer
-     *     Rate of video bandwidth is kilobits per second
+    /**
+     * Sets the video bandwidth.
      *
-     *   Sets the video bandwidth.
+     * @param {Number} kbitsPerSecond - Rate
+     * of video bandwidth in kilobits per second.
+     * @returns {undefined} undefined
+     * @public
      */
     this.setVideoBandwidth = function(kbitsPerSecond) {
-       easyrtc.setVideoBandwidth(kbitsPerSecond);
+        easyrtc.setVideoBandwidth(kbitsPerSecond);
     };
 
-    /* Parameters:
-     *   state : boolean
-     *     The new state of the camera.
+    /**
+     * Enables or disables the camera based on the value of `state`.
      *
-     * Enables or disables the camera based off the value of state.
+     * @param {Boolean} state - The new state of the camera.
+     * @returns {undefined} undefined
+     * @public
      */
     this.enableCamera = function(state) {
         easyrtc.enableCamera(state);
     };
 
-    /* Parameters:
-     *   state : boolean
-     *     The new state of the microphone.
+    /**
+     * Enables or disables the microphone based on the value of `state`.
      *
-     * Enables or disables the microphone based off of the value of state.
+     * @param {Boolean} state - The new state of the microphone.
+     * @returns {undefined} undefined
+     * @public
      */
     this.enableMicrophone = function(state) {
         easyrtc.enableMicrophone(state);
     };
 
-    /* Parameters:
-     *   dest      : Object
-     *     A Object that should contain at just one of the following fields:
-     *       rtcId : String
-     *       room  : String
+    /**
+     * Sends a message to another peer.
      *
-     *   msgType   : String
-     *     Type of message (specific to the application)
-     *
-     *   msgData   : Object
-     *     Message contents (must be JSON-able)
-     *
-     *   successFn : function(msgType, msgData)
-     *     Callback function with server response results
-     *
-     * This sends a message to another peer.
+     * @param {Object} dest - An object describing the message
+     * destination. It should contain just one of the following:
+     *     rtcId: String
+     *     room: String
+     * @param {String} msgType - Type of message (specific to the application).
+     * @param {Object} msgData - Message contents (must be JSON-able).
+     * @param {Function} successFn - Callback function with server response
+     * results. It takes two arguments: a msgType and some msgData.
+     * @returns {Boolean} True on success, false otherwise.
+     * @public
      */
     this.sendPeerMessage = function(dest, msgType, msgData, successFn) {
         var target = {};
@@ -135,29 +164,25 @@ var VTCClient = function(myId, roomName, onErrorFn) {
         return true;
     };
 
-    /* Returns:
-     *   MediaStream
-     *     MediaStream object for the local media sources.
-     *
+    /**
      * Obtains and returns the requested (via enableAudio/enableVideo) media sources.
+     * @returns {Object} MediaStream object for the local media sources.
+     * @public
      */
     this.getLocalStream = function() {
         return easyrtc.getLocalStream();
     };
 
-    /* Parameters:
-     *   videoSel : jQuery
-     *     This is a jQuery selector for a video element. This *must* be a jQuery selector because
-     *     we call .get(0) on it in this function.
+    /**
+     * Binds stream to the provided videoSel.
+     * NOTE: Upon any DOM element manipulation of the video element, the video stream
+     * will pause. This can be remedied by calling .load() on the raw video DOM element.
      *
-     *   stream : MediaStream
-     *     This contains the MediaStream object in which videoSel will be bound to.
-     *
-     * This functions binds stream to the provided videoSel.
-     *
-     * HINT:
-     *   Upon any DOM element manipulation of the video element, the video stream will pause. This
-     *   can be remedied by calling .load() on the raw video DOM element.
+     * @param {Object} videoSel - A jQuery selector for a video element. This *must* be
+     * a jQuery selector because we call .get(0) on it in this function.
+     * @param {Object} stream - MediaStream object to which videoSel will be bound.
+     * @returns {undefined} undefined
+     * @public
      */
     this.setVideoObjectSrc = function(videoSel, stream) {
         easyrtc.setVideoObjectSrc(videoSel.get(0), stream);
@@ -168,10 +193,10 @@ var VTCClient = function(myId, roomName, onErrorFn) {
 
 var VTCCore = {
     // This function is an user-defined error handler.
-    // Type: function(Object({title: String, content: String}))
+    // Type: function(Object({ title: String, content: String }))
     _errorFn: null,
 
-    // Contains the configuration of the media devices
+    // Contains the configuration of the media devices.
     // Object structure:
     //   {
     //     cameraIsEnabled : <boolean>,
@@ -179,18 +204,30 @@ var VTCCore = {
     //   }
     config: null,
 
-    // An instantiation of VTCClient
+    // An instantiation of VTCClient.
     client: null,
 
-    // A function that checks browser support for WebRTC API. This can be called without
-    // calling VTCCore.initialize
+    /**
+     * Checks browser support for the WebRTC API. This
+     * can be called without calling VTCCore.initialize().
+     *
+     * @returns {Boolean} True if WebRTC is supported,
+     * false otherwise.
+     * @public
+     */
     isBrowserSupported: function() {
         return easyrtc.supportsGetUserMedia() && easyrtc.supportsPeerConnections();
     },
 
-    // Validates a configuration Object. If this function is given an argument, it will
-    // check to ensure config is a valid configuration Object. Otherwise, it will check
-    // to see if VTCCore.config is a valid configuration Object.
+    /**
+     * Validates a configuration object. If this function is given an argument, it
+     * will check to ensure the config is valid. Otherwise, it will check to see if
+     * VTCCore.config is a valid configuration object.
+     *
+     * @param {Object} config - Configuration object.
+     * @returns {Boolean} True if config is valid, false otherwise.
+     * @private
+     */
     _validateConfig: function(config) {
         if (config === undefined) {
             config = this.config;
@@ -208,12 +245,18 @@ var VTCCore = {
         }
     },
 
-    /* Parameters:
-     *   config : Object({
-     *              cameraIsEnabled : <boolean>,
-     *              micIsEnabled    : <boolean>
-     *            })
-     *     Contains three fields denoting the initial state of the media devices.
+    /**
+     * Initializes the VTCCore instance.
+     *
+     * @param {Object} config - Configuration object of the shape:
+     *   {
+     *       cameraIsEnabled : <boolean>,
+     *       micIsEnabled    : <boolean>
+     *   }
+     * Contains three fields denoting the initial state of the media devices.
+     * @returns {Object} Null if the config is invalid; otherwise,
+     * returns the current instance (`this`).
+     * @public
      */
     initialize: function(config) {
         var _this = this;
@@ -246,14 +289,17 @@ var VTCCore = {
         return this;
     },
 
-    /* Parameters:
-     *   peerMessageFn : function (client : VTCClient,
-     *                             peerId : String,
-     *                             msgType : String,
-     *                             content : Object)
-     *     This is a callback function that is called when a peer message arrives.
+    /**
+     * Sets a custom handler for messages sent by other peers.
      *
-     * This function sets a custom handler for messages sent by other peers.
+     * @param {Function} peerMessageFn - A callback function
+     * that is called when a peer message arrives. It's of the form:
+     * function(client  : VTCClient,
+     *          peerId  : String,
+     *          msgType : String,
+     *          content : Object)
+     * @returns {Object} The VTCCore instance.
+     * @public
      */
     onPeerMessage: function(peerMessageFn) {
         var _this = this;
@@ -264,13 +310,16 @@ var VTCCore = {
         return this;
     },
 
-    /* Parameters:
-     *   streamAcceptFn : function (client : VTCClient,
-     *                              peerId : String,
-     *                              stream : MediaStream)
-     *     This is a callback function that is called when a new stream is "accepted"
+    /**
+     * Sets a custom handler to receive streams from other peers.
      *
-     *   This function sets a custom handler to receive streams from other peers.
+     * @param {Function} streamAcceptFn - A callback function that
+     * is called when a new stream is "accepted". It's of the form:
+     * function(client : VTCClient,
+     *          peerId : String,
+     *          stream : MediaStream)
+     * @returns {Object} The VTCCore instance.
+     * @public
      */
     onStreamAccept: function(streamAcceptFn) {
         var _this = this;
@@ -281,12 +330,15 @@ var VTCCore = {
         return this;
     },
 
-    /* Parameters:
-     *   streamClose : function(client : VTCClient,
-     *                          peerId : String)
-     *     This is a callback function that is called when a stream is closed.
+    /**
+     * Handles stream close events.
      *
-     *   Handles stream close events.
+     * @param {Function} streamCloseFn - A callback function
+     * that is called when a stream is closed. It's of the form:
+     * function(client : VTCClient,
+     *          peerId : String)
+     * @returns {Object} The VTCCore instance.
+     * @public
      */
     onStreamClose: function(streamCloseFn) {
         var _this = this;
@@ -297,31 +349,38 @@ var VTCCore = {
         return this;
     },
 
-    /* Parameters:
-     *   errorFn : function(Object({
-     *               title   : String,
-     *               content : String<HTML>
-     *             })
-     *     Callback that is called when an error condition arises
+    /**
+     * Error handler.
+     *
+     * @param {Function} errorFn - Callback that is called
+     * when an error condition arises. It's of the form:
+     * function(Object({
+     *              title   : String,
+     *              content : String<HTML>
+     *          })
+     * @returns {Object} The VTCCore instance.
+     * @public
      */
     onError: function(errorFn) {
         this._errorFn = errorFn;
         return this;
     },
 
-    // Parameters:
-    //   userName : String
-    //     The name of the connecting user.
-    //
-    //   roomName : String
-    //     The name of the room to be joined.
-    //
-    //   successFn : function(VTCClient)
-    //     The callback function to be called upon successfully joining the room.
-    //
-    // Before calling this function, it is recommended to have already called onPeerMessage, onStreamAccept, and
-    // onStreamClose with appropriate callback functions. It is possible that setting the callbacks after invoking
-    // easyrtc.connect might cause events to be lost.
+    /**
+     * Connects to the API. Before calling this function, it is recommended
+     * to have already called onPeerMessage, onStreamAccept, and onStreamClose
+     * with appropriate callback functions. It is possible that setting the
+     * callbacks after invoking easyrtc.connect might cause events to be lost.
+     *
+     * @param {String} userName - The name of the connecting user.
+     * @param {String} roomName - The name of the room to be joined.
+     * @param {Function} successFn - The callback function to be called
+     * upon successfully joining the room. It's of the form:
+     * function(VTCClient)
+     * @returns {Object} Null if there was a connection error; otherwise,
+     * returns the VTCCore instance.
+     * @public
+     */
     connect: function(userName, roomName, successFn) {
         if (!this._validateConfig()) {
             ErrorMetric.log('VTCCore.connect => config changed somehow...');
@@ -367,9 +426,9 @@ var VTCCore = {
                 if (peersCount < kMaxCallersPerRoom) {
                     callPeers(peersToCall.length - 1);
                 } else {
-                    // NOTE(security): This check and many others do not prevent users from force joining
-                    //                 a room by running JavaScript. It might be a good idea in the future
-                    //                 to enforce these limits of the server side.
+                    // NOTE (security): This check and many others do not prevent users from force joining
+                    //                  a room by running JavaScript. It might be a good idea in the future
+                    //                  to enforce these limits of the server side.
                     if (_this._errorFn !== undefined) {
                         _this._errorFn({
                             title: 'Room "' + roomName + '" is full.',
@@ -396,7 +455,7 @@ var VTCCore = {
                 ErrorMetric.log('easyrtc.connect => failed to connect');
                 ErrorMetric.log('                => ' + errorCode + ': ' + errorText);
 
-                // FIXME: proofread and make this text better
+                // @todo FIXME: proofread and make this text better
                 if (_this._errorFn !== undefined) {
                     _this._errorFn({
                         title: 'An Error Has Occurred',
@@ -410,7 +469,7 @@ var VTCCore = {
             ErrorMetric.log('easyrtc.initMediaSource => unable to initialize media source');
             ErrorMetric.log('                        => ' + errorCode + ': ' + errorText);
 
-            // FIXME: proofread and make this text better
+            // @todo FIXME: proofread and make this text better
             if (_this._errorFn !== undefined) {
                 _this._errorFn({
                     title: 'Unable to Initialize Media Sources',
@@ -425,11 +484,23 @@ var VTCCore = {
         return this;
     },
 
-    // Returns the VTCClient instance
+    /**
+     * Returns the VTCClient instance.
+     *
+     * @returns {Object} The VTCClient instance.
+     * @public
+     */
     getClient: function() {
         return this.client;
     },
 
+    /**
+     * Cleans up (hangs up, leaves the
+     * room, closes the connection, etc).
+     *
+     * @returns {undefined} undefined
+     * @public
+     */
     finalize: function() {
         var client = this.client;
         if (client !== null) {
